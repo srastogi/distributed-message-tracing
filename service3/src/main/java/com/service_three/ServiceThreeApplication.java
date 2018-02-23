d503742aeecd317960e01ee0b358637a13a57db7
Shikhar Rastogi <shikhar.whitepaw@gmail.com>
2018-02-22 22:39:54 -0600

    Setup repo

---
+++ service3/src/main/java/com/service_three/ServiceThreeApplication.java
@@ -0,0 +1,12 @@
+package com.service_three;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceThreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceThreeApplication.class, args);
+    }
+}
